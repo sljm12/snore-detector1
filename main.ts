3db5e1feb6c24e9f9975289ec33c803a56463741
sljm12 <leejianm@hotmail.com>
2021-11-18 13:14:51 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    basic.showNumber(numtimes)
+    basic.pause(2000)
+})
 input.onSound(DetectedSound.Loud, function () {
     numtimes += 1
+    showDots()
+})
+function showDots () {
+    basic.clearScreen()
     numLines = Math.floor(numtimes / 5)
     serial.writeValue("numtimes", numtimes)
     serial.writeValue("numLines", numLines)
@@ -11,14 +20,14 @@ input.onSound(DetectedSound.Loud, function () {
             }
         }
     }
-    for (let X = 0; X <= numtimes % 5; X++) {
+    for (let X = 0; X <= numtimes % 5 - 1; X++) {
         led.plot(X, numLines)
     }
-})
+}
 let numLines = 0
 let numtimes = 0
-numtimes = 0
 input.setSoundThreshold(SoundThreshold.Loud, 50)
+numtimes = 0
 basic.forever(function () {
     radio.setGroup(1)
 })
